f11be3ba5019deb6042ff050d2826eab1f710493
skalyan04 <123116625+skalyan04@users.noreply.github.com>
2024-06-21 20:51:52 -0600

    Update constructorChampTablePull.sh
    Now pulls data from startYear to endYear and can then individually pull each year and save it as a JSON file. When you concatenate one of the season JSON files, it will print everything as is (like HTML), but I still cannot get it to print the information from a table for the Constructor's Table information.

--- constructorChampTablePull.sh
+++ constructorChampTablePull.sh
@@ -20,3 +20,37 @@ for year in {$startYear..$endYear}; do
 done
 
 jq . '.MRData.RaceTable.Races{} | {raceName: .racename, date: .date}' "${year}_data.json"
+
+
+
+#####################################################################################
+## Update by SREYA KALYAN
+## Posted on June 21, 2024
+
+#!/bin/bash
+
+# Check if the correct number of arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <startYear> <endYear>"
+    exit 1
+fi
+
+# Assign the arguments to variables
+startYear=$1
+endYear=$2
+
+# Use the variables
+echo "Fetching Constructor's Championship Table from $startYear to $endYear..."
+# Your script logic here
+
+
+for year in $(seq $startYear $endYear); do
+  curl -o "${year}_season.json" "http://ergast.com/api/f1/${year}.json"
+done
+
+for year in $(seq $startYear $endYear); do
+        echo "Year: $year"
+        jq '.MRData.ConstructorTable.Constructors[] | {constructorName: .name, nationality: .nationality}' "${year}_season.json"
+done
+
+#####################################################################################
