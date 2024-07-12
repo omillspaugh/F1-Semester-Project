10faeaa1ccd95aa24a70ad2316ee0ee8a325b1a1
Ashaman123 <odogb02@gmail.com>
2024-07-12 07:46:53 -0600

    commit

--- install_packages.sh
+++ install_packages.sh
@@ -4,10 +4,11 @@
 sudo apt install -y jq &
 sudo apt install -y sysvbanner &
 sudo apt install -y texlive-latex-base &
-sudo apt install -y dos2unix
-sudo apt-get install poppler-utils
+sudo apt install -y dos2unix &
+sudo apt-get install poppler-utils &
 
-dos2unix mainMenuF1.sh constructorChampTablePull.sh driverChampTablePull.sh driverListPull.sh constructorListPull.sh
+# Convert all files to the same file endings for consistency
+dos2unix mainMenuF1.sh constructorChampTablePull.sh driverChampTablePull.sh driverListPull.sh constructorListPull.sh &
 
 # Wait for all background processes to finish
 wait
