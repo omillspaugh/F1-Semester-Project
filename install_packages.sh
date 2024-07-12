b01b80877ec40874a25511245f4942570f3bc572
Owen Millspaugh <omillspa@uccs.edu>
2024-07-12 08:18:58 -0600

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -4,9 +4,10 @@
 sudo apt install -y jq &
 sudo apt install -y sysvbanner &
 sudo apt install -y texlive-latex-base &
-sudo apt install -y dos2unix
-sudo apt-get install poppler-utils
+sudo apt install -y dos2unix &
+sudo apt-get install poppler-utils &
 
+# Used to make sure files all end in same ending when switching between different systems
 dos2unix mainMenuF1.sh constructorChampTablePull.sh driverChampTablePull.sh driverListPull.sh constructorListPull.sh
 
 # Wait for all background processes to finish
