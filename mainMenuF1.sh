c5d6f8e066894d027c559e1b82f06cab560e7470
prasadtara <79865246+prasadtara@users.noreply.github.com>
2024-07-15 20:35:16 -0600

    Update mainMenuF1.sh

--- mainMenuF1.sh
+++ mainMenuF1.sh
@@ -14,64 +14,26 @@ fi
 
 clear
 
-# Print welcome message and prompt user to input years
-echo "Welcome to the Formula 1 Data Aggregator! This will present data from 2018 to 2022."
-echo "Please enter a year range:"
+# Print welcome message and prompt user to choose a selection
+echo "Welcome to the Formula 1 Data Aggregator!"
+echo "Please choose menu option."
 
-# Checks if input year is valid (4 digits, returns 1 and prints error message if invalid, returns 0 if valid)
-# Also checks if input years are between 2018 and 2022
-function validateYear() {
-  year="$1"
-  if [[ ! "$year" =~ ^[0-9]{4}$ ]]; then
-    echo "Invalid year. Please enter a 4-digit year."
-    return 1
-  elif [[ "$year" -lt 2018 || "$year" -gt 2022 ]]; then
-    echo "Invalid year. Please enter a year between 2018 and 2022."
-    return 1
-  fi
-  return 0
-}
-
-# Loop to prompt user to enter start year and continues until a valid year is entered
-while true; do
-  read -p "Starting Year: " startYear
-  if validateYear "$startYear"; then
-    break
-  fi
-done
-
-# Loop to prompt user to enter end year and continues until a valid year is entered
-while true; do
-  read -p "Ending Year: " endYear
-  if validateYear "$endYear" && [[ $endYear -ge $startYear ]]; then
-    break
-  else
-    echo "Invalid end year. Must be greater than or equal to start year."
-  fi
-done
-
-# Prints menu options, prompts user to select one, and continues to prompt until valid selection is made
-echo
-echo "Looking at the year range $startYear to $endYear, I can provide the following data:"
-echo
+# Menu options, prompts user to select one, and continues to prompt until valid selection is made
+echo 
 menuSelect=0
 while [[ $menuSelect -lt 1 || $menuSelect -gt 5 ]]; do
-  echo "1. 2022 Driver Number List"
-  echo "2. 2021 Driver Number List"
-  echo "3. 2020 Driver Number List"
-  echo "4. 2019 Driver Number List"
-  echo "5. 2018 Driver Number List"
-  echo "6. General Info about Formula 1"
+  echo "1. Current driver list
+  echo "2. General Info about Formula 1"
   read -p "Please enter the corresponding number of the data choice you would like to view: " menuSelect
-  if [[ $menuSelect -lt 1 || $menuSelect -gt 5 ]]; then
-    echo "Invalid selection. Please enter a number between 1 and 5."
+  if [[ $menuSelect -lt 1 || $menuSelect -gt 2 ]]; then
+    echo "Invalid selection. Please enter either 1 or 2."
   fi
 done
 
 # Switch case to run the script for each selection, prints error message if script fails
 case $menuSelect in
   1)
-    scriptName="$DRIVER_SEARCH_BASE/driverSearch2022.sh"
+    scriptName="$DRIVER_SEARCH_BASE/driverSearch.sh"
     if [ ! -f "$SCRIPT" ]; then
       echo "Error: Script '$SCRIPT' not found."
       exit 1
@@ -80,42 +42,6 @@ case $menuSelect in
       echo "Error: '$SCRIPT' failure."
     fi
     ;;
-  2)
-    scriptName="$DRIVER_SEARCH_BASE/driverSearch2021.sh"
-    if [ ! -f "$SCRIPT" ]; then
-      echo "Error: Script '$SCRIPT' not found."
-      exit 1
-    fi
-    if ! "$SCRIPT" "$startYear" "$endYear"; then
-      echo "Error: '$SCRIPT' failure."
-    fi
-  3)
-      scriptName="$DRIVER_SEARCH_BASE/driverSearch2020.sh"
-    if [ ! -f "$SCRIPT" ]; then
-      echo "Error: Script '$SCRIPT' not found."
-      exit 1
-    fi
-    if ! "$SCRIPT" "$startYear" "$endYear"; then
-      echo "Error: '$SCRIPT' failure."
-    fi
-  4)
-    scriptName="$DRIVER_SEARCH_BASE/driverSearch2019.sh"
-    if [ ! -f "$SCRIPT" ]; then
-      echo "Error: Script '$SCRIPT' not found."
-      exit 1
-    fi
-    if ! "$SCRIPT" "$startYear" "$endYear"; then
-      echo "Error: '$SCRIPT' failure."
-    fi
-  5)
-    scriptName="$DRIVER_SEARCH_BASE/driverSearch2018.sh"
-    if [ ! -f "$SCRIPT" ]; then
-      echo "Error: Script '$SCRIPT' not found."
-      exit 1
-    fi
-    if ! "$SCRIPT" "$startYear" "$endYear"; then
-      echo "Error: '$SCRIPT' failure."
-    fi
   6)
     if ! "$GENERAL_INFO_SCRIPT"; then
       echo "Error: Failed to generate general information."
